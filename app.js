70f9f9fbd1e71689c341bc5e8ad166d85d4c9b25
USER <user@USERs-iMac-2.local>
2020-08-20 20:58:32 -0600

    completed the assignment

---
+++ app.js
@@ -0,0 +1,5 @@
+var tweets = ['Not sure which is harder on a relationship: sharing a dresser for three years or sharing an iPhone charger for one day.', 'You can fail at what you don’t want—so you might as well take a chance at doing what you love.', 'Whoever established the high road and how high it should be should be fired', 'Instant gratification takes too long', 'I admit that I live in the past, but only because housing is so much cheaper.', 'One of my biggest fears is that I’ll marry into a family that runs 5Ks on holidays.', 'Mapquest really needs to start its directions on number five. Pretty sure I know how to get out of my neighborhood.', 'My parents said marrying was an optimistic thing to do in pessimistic times', 'If you are not yelling at your kids, you are not spending enough time with them.', 'It is better to live one day as a lion than 100 years as a sheep.'];
+arraylength= tweets.length;
+for(index=0; index<arraylength; index++){
+    console.log(tweets[index])
+}
